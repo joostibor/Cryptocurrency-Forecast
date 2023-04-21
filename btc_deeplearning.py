a9b96507e1da3125e98bc61a83346d669d9b10ca
joostibor98 <joostibor98@gmail.com>
2023-04-21 11:33:38 +0200

    Sok változás, elfelejtettem committolni Függvényekbe rendezés MinMaxScaling rework

--- btc_deeplearning.py
+++ btc_deeplearning.py
@@ -1,23 +1,59 @@
+import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv('BTC-USD.csv')
 
+#Bemeneti dataset alap normalizálása
 def normalize(dataframe):
     #Felesleges oszlopok törlése
     del dataframe['Adj Close']
     del dataframe['Date']
     
     #Átlagár oszlop hozzáadása
-    avg_price = [(row['Open'] + row['Close'] + row['High'] + row['Low'])/4 for index, row in df.iterrows()]
+    avg_price = [(row['Open'] + row['Close'] + row['High'] + row['Low'])/4 for index, row in dataframe.iterrows()]
     dataframe['Avg'] = avg_price
 
-    #Min-Max normlizáció
+#Dataset szétosztása training és test setre a forrás pszeudokódja alapján
+#A kiválasztott érték a tanításhoz az avg
+def split():
+    training = []
+    test = []
+    div_ratio = 0.8
+    df_elemenets_count = len(df.index)
+    div_at = int(df_elemenets_count * div_ratio)
+    for index, row in df.iterrows():
+        if index < div_at:
+            training.append(df.iloc[index].at['Avg'])
+        else:
+            test.append(df.iloc[index].at['Avg'])
+    return(training, test)
+
+#MinMax normálizálás
+def MinMaxScale(array):
+    lenght = len(array)
+    arr = np.array(array).reshape(lenght, 1)
     scaler = MinMaxScaler()
-    normalized_data = scaler.fit_transform(dataframe)
-    normalized_df = pd.DataFrame(normalized_data, columns=dataframe.columns)
+    scaled_set = scaler.fit_transform(arr)
+    return scaled_set
 
-    return normalized_df
+def split2(dataframe, pastdays):
+    training = []
+    test = []
+    for index, row in df.iterrows():
+        outindex = index + pastdays
+        if outindex > df.shape[0] - 1:
+            break
+        temp_training = dataframe.iloc[index:outindex].at['Avg']
+        temp_test = dataframe.iloc[outindex].at['Avg']
+        training.append(temp_training)
 
-df = normalize(df)
-print(df)
+normalize(df)
+training_set, test_set = split()
+training_set = MinMaxScale(training_set)
+test_set = MinMaxScale(test_set)
+
+pastdays = 30
+print(training_set)
+print("------------------------------------------------------------------------------------------------")
+print(test_set)
