cf7fd66e043eddf49165151a72ada14388240bfc
joostibor98 <joostibor98@gmail.com>
2023-04-21 10:00:24 +0200

    Min-Max normalizáció hozzáadása

--- btc_deeplearning.py
+++ btc_deeplearning.py
@@ -1,12 +1,23 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv('BTC-USD.csv')
 
 def normalize(dataframe):
+    #Felesleges oszlopok törlése
     del dataframe['Adj Close']
     del dataframe['Date']
+    
+    #Átlagár oszlop hozzáadása
     avg_price = [(row['Open'] + row['Close'] + row['High'] + row['Low'])/4 for index, row in df.iterrows()]
     dataframe['Avg'] = avg_price
 
-normalize(df)
+    #Min-Max normlizáció
+    scaler = MinMaxScaler()
+    normalized_data = scaler.fit_transform(dataframe)
+    normalized_df = pd.DataFrame(normalized_data, columns=dataframe.columns)
+
+    return normalized_df
+
+df = normalize(df)
 print(df)
