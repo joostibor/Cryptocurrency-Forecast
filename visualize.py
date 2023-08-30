356eb40c0c02b28fb7f3c25c6399da5adba812b3
joostibor98 <joostibor98@gmail.com>
2023-08-30 11:45:42 +0200

    Fájlok rendezése Megjelenítés finomítása

--- visualize.py
+++ visualize.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 
 p_data = pd.read_csv('BTC-USD_predict_wfdl.csv', index_col='Date', parse_dates=['Date'])
-f_data = pd.read_csv('BTC-USD_fact.csv', index_col='Date', parse_dates=['Date'])
+f_data = pd.read_csv('BTC-USD.csv', index_col='Date', parse_dates=['Date'])
 
-predict_data = p_data['2022':]
-predict_prices = predict_data['Close'].values
-fact_data = f_data['2022':]
-fact_prices = fact_data['Close'].values
+predict_data = p_data['2021':]
+fact_data = f_data['2021':]
 
-plt.plot(predict_prices, color='green', label='Modell alapján előrejelzett napi záró árfolyam')
-plt.plot(fact_prices, color='black', label='Valós napi záró árfolyamok')
+plt.plot(p_data['2021':]['Close'], color='green', label='Modell alapján előrejelzett napi záró árfolyam')
+plt.plot(fact_data['2021':]['Close'], color='black', label='Valós napi záró árfolyamok')
 plt.title('BTC záró árfolyam előrejelzés LSTM-GRU hibrid modellel')
-plt.xlabel('Dátum')
-plt.ylabel('Ár')
+plt.xticks(rotation=45, fontsize=8)
+plt.ylabel('Ár ($)')
 plt.legend(loc='upper left')
+plt.savefig('test.svg', format='svg')
 plt.show()
