60bf7bf56cd330e960d555e56ad7a1eefb17a2e0
joostibor98 <joostibor98@gmail.com>
2023-10-12 10:25:16 +0200

    Kriptovaluta loop elkészítése

--- svm_pso.py
+++ svm_pso.py
@@ -6,47 +6,44 @@ from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 from datetime import datetime, timedelta
 
-#cryptos = ['BTC'] #Előrejelzendő kriptovaluták
+def dataPreprocessingAndSvmTeaching(dataframe, rbf):
+    dataframe['Prediction'] = dataframe[['Close']].shift(-forecasting_days) #Új oszlop beszúrása az előrejelzendő napok számával megegyező eltolással
+    x = np.array(df.drop(['Date', 'Open', 'High', 'Low', 'Volume', 'Prediction'], axis=1)) #Close oszlop átkonvertálás numpy tömbbé
+    x = x[:len(df)-forecasting_days] #x tömb méretének csökkentése az előjelzendő napok számával
+    y = np.array(df['Prediction']) #Újonnan beszúrt oszlop konvertálása numpy tömbbé
+    y = y[:-forecasting_days] #Üres sorok kihagyása
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2) #Adatok szétválasztása -> 80% tanító, 20% tesztelő
+    rbf.fit(x_train, y_train) #SVM tanítása
+    #Pontosság vizsgálat
+    model_accuracy = rbf.score(x_test, y_test)
+    print("Modell pontossága: ", model_accuracy)
+    #Teszthalmazon átlagos négyzetes eltérés mérése
+    predictions = rbf.predict(x_test)
+    rmse = (np.sqrt(np.mean(np.square((y_test - predictions) / y_test)))) * 100
+    print("Átlagos négyzetes eltérés (%): ", rmse)
+    #DataFrame visszaadása
+    return dataframe
+
 forecasting_days = 30 #Előrejelzendő napok száma
-refreshExchangeRate(cryptos) #Árfolyamadatok frissítése
-df = pd.read_csv('.\Exchange Rates\BTC-USD_fact.csv', index_col=False) #Adatfájl betöltése
-df['Prediction'] = df[['Close']].shift(-forecasting_days) #Új oszlop beszúrása az előrejelzendő napok számával megegyező eltolással
-x = np.array(df.drop(['Date', 'Open', 'High', 'Low', 'Volume', 'Prediction'], axis=1)) #Close oszlop átkonvertálás numpy tömbbé
-x = x[:len(df)-forecasting_days] #x tömb méretének csökkentése az előjelzendő napok számával
-y = np.array(df['Prediction']) #Újonnan beszúrt oszlop konvertálása numpy tömbbé
-y = y[:-forecasting_days] #Üres sorok kihagyása
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2) #Adatok szétválasztása -> 80% tanító, 20% tesztelő
-
-#SVM elkészítése
+#refreshExchangeRate(cryptos) #Árfolyamadatok frissítése
 rbf = SVR(kernel='rbf', C=1e3, gamma=0.00001) #Árfolyamra való tekintettel rbf függvény használata
-rbf.fit(x_train, y_train) #SVM tanítása
-model_accuracy = rbf.score(x_test, y_test)
-print("Modell pontossága: ", model_accuracy)
 
-#SVM tesztelése és összehasonlítása a tényadatokkal
-print("----------Előrejelzés-Teszt fact adatok----------")
-predictions = rbf.predict(x_test)
-for i in range(0, len(predictions)):
-    print(f'{predictions[i]},{y_test[i]}')
+for c in cryptos:
+    df = pd.read_csv(f'.\Exchange Rates\{c}-USD_fact.csv', index_col=False) #Adatfájl betöltése
+    df = dataPreprocessingAndSvmTeaching(df, rbf)
 
-firsttime = True
+    firsttime = True #.csv fájl import miatt létrehozott változó
 
-#Előrejelzés 30 napra
-for i in range(0, 30):
-    if not firsttime:
-        df = pd.read_csv('.\Exchange Rates\BTC-USD_p_osvm.csv', index_col=False)
-        df['Prediction'] = df[['Close']].shift(-forecasting_days) #Új oszlop beszúrása az előrejelzendő napok számával megegyező eltolással
-        x = np.array(df.drop(['Date', 'Open', 'High', 'Low', 'Volume', 'Prediction'], axis=1)) #Close oszlop átkonvertálás numpy tömbbé
-        x = x[:len(df)-forecasting_days] #x tömb méretének csökkentése az előjelzendő napok számával
-        y = np.array(df['Prediction']) #Újonnan beszúrt oszlop konvertálása numpy tömbbé
-        y = y[:-forecasting_days] #Üres sorok kihagyása
-        x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2) #Adatok szétválasztása -> 80% tanító, 20% tesztelő
-        rbf.fit(x_train, y_train) #SVM tanítása
-    last_rows = np.array(df.drop(['Date', 'Open', 'High', 'Low', 'Volume', 'Prediction'], axis=1))[-forecasting_days:] #-->forecasting days helyett 1, és akkor 1 napot lehet előre és ezt kell majd for ciklusozni, try this shit
-    forecast = rbf.predict(last_rows)
-    df = df.drop(['Prediction'], axis=1)
-    df.loc[len(df)] = [str((datetime.strptime(df.iloc[-1]['Date'], '%Y-%m-%d %H:%M:%S%z') + timedelta(days=1))), forecast[forecasting_days-1], forecast[forecasting_days-1], forecast[forecasting_days-1], forecast[forecasting_days-1], 0]
-    df.to_csv('.\Exchange Rates\BTC-USD_p_osvm.csv', index=False)
-    firsttime = False
+    #Előrejelzés 30 napra
+    for i in range(0, 30):
+        if not firsttime:
+            df = pd.read_csv(f'.\Exchange Rates\{c}-USD_p_osvm.csv', index_col=False)
+            df = dataPreprocessingAndSvmTeaching(df, rbf)
+        last_rows = np.array(df.drop(['Date', 'Open', 'High', 'Low', 'Volume', 'Prediction'], axis=1))[-forecasting_days:] #-->forecasting days helyett 1, és akkor 1 napot lehet előre és ezt kell majd for ciklusozni, try this shit
+        forecast = rbf.predict(last_rows)
+        df = df.drop(['Prediction'], axis=1)
+        df.loc[len(df)] = [str((datetime.strptime(df.iloc[-1]['Date'], '%Y-%m-%d %H:%M:%S%z') + timedelta(days=1))), forecast[forecasting_days-1], forecast[forecasting_days-1], forecast[forecasting_days-1], forecast[forecasting_days-1], 0]
+        df.to_csv(f'.\Exchange Rates\{c}-USD_p_osvm.csv', index=False)
+        firsttime = False
 
-#Több kriptos ciklus kell + a grafikonok
+#Grafikonok!
