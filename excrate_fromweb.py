71153fcfd9c4a437b22902a2fb46bfa31117e5f5
joostibor98 <joostibor98@gmail.com>
2023-12-04 20:54:41 +0100

    Végső állapotot és programkódok feltöltése

--- excrate_fromweb.py
+++ excrate_fromweb.py
@@ -5,6 +5,6 @@ cryptos = ['BTC', 'ETH', 'BNB', 'DOGE', 'LTC'] #Bitcoin, Ethereum, Binance, Doge
 
 for i in range (0,len(cryptos)):
     crypto_exc = yf.Ticker(f'{cryptos[i]}-USD')
-    hist = crypto_exc.history(start="2009-01-03", end=dt.datetime.now()) #Kezdődátum megegyezik a Bitcoin indulási idejével
+    hist = crypto_exc.history(start="2009-01-03", end="2023-11-11") #Kezdődátum megegyezik a Bitcoin indulási idejével
     hist.drop(hist.columns[[5, 6]], axis=1, inplace=True) #Felesleges oszlopok törlése
-    hist.to_csv(f'.\Exchange Rates\{cryptos[i]}-USD_fact_1201.csv', sep=',',index=True)
+    hist.to_csv(f'.\Exchange Rates\{cryptos[i]}-USD_fact_1111.csv', sep=',',index=True)
--- graphmaker.py
+++ graphmaker.py
@@ -4,19 +4,17 @@ import matplotlib.pyplot as plt
 from lstm_gru import cryptos
 
 for c in cryptos:
-    f_data = pd.read_csv(f'.\Exchange Rates\{c}-USD_fact_1201.csv', index_col='Date', parse_dates=['Date'])
-    #fact_prices = f_data['2023-10-13':]['Close'].values
+    f_data = pd.read_csv(f'.\Exchange Rates\{c}-USD_fact_1111.csv', index_col='Date', parse_dates=['Date'])
 
-    p_data = pd.read_csv(f'.\Exchange Rates\{c}-USD_p_olstm_wfdl.csv', index_col='Date', parse_dates=['Date'])
-    #predicted_data = p_data['2023-10-13':]['Close'].values
+    p_data = pd.read_csv(f'.\Exchange Rates\{c}-USD_p_svm_evolution.csv', index_col='Date', parse_dates=['Date'])
 
     plt.clf()
     plt.plot(f_data['2023-10-13':]['Close'].values, color='black', label='Valós napi záróárfolyam')
     plt.plot(p_data['2023-10-13':]['Close'].values, color='green', label='Modell által előrejelzett árfolyam')
-    plt.title(f'LSTM modell árfolyam előrejelzés validációja {c} kriptovalután')
+    plt.title(f'Evolúciós algoritmussal optimalizált SVM modell\n validációja {c} kriptovalután')
     plt.xticks(rotation=45, fontsize=8)
     plt.xlabel('Jósolt napok száma')
     plt.ylabel('Ár ($)')
     plt.legend(loc='upper left')
-    plt.savefig(f'.\SVG Format Figures\{c}-USD_predict_valid_olstm_wfdl.svg', format='svg')
+    plt.savefig(f'.\SVG Format Figures\{c}-USD_predict_valid_svm_evolution.svg', format='svg')
     plt.show()
